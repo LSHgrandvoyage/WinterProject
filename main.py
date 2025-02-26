038bee1474c1ffeb2001353c45a0b2c856d964ca
LSHgrandvoyage <2718sh0517@gm.gist.ac.kr>
2025-02-27 02:19:17 +0900

    Can have many keywords & Speed optimization

--- main.py
+++ main.py
@@ -10,46 +10,47 @@ from bs4 import BeautifulSoup
 app = FastAPI()
 
 # crawler function
-def crawl_news(keyword: str):
+def crawl_news(keywords: list):
     # Set the Chrome driver options
     options = Options()
-    options.add_argument("--headless")  # 브라우저 창을 띄우지 않음
-    options.add_argument("--disable-gpu")  # GPU 사용 안 함
-    options.add_argument("window-size=1920x1080")  # 화면 크기 설정
-
-    driver = webdriver.Chrome()
+    options.add_argument("--headless") # Optimization 1, Don't print the browser on my screen
+    options.add_argument("--disable-gpu") # Optimization 2
+    options.add_argument("--disable-extensions") # Optimization 3
+    driver = webdriver.Chrome(options=options)
 
     # time calculation to set the finding time
     current_time = datetime.now()
     start = (current_time - timedelta(hours=4)).strftime("%Y.%m.%d.%H.%M")
     end = current_time.strftime("%Y.%m.%d.%H.%M")
 
-    url = f"https://search.naver.com/search.naver?where=news&query={keyword}&sm=tab_opt&sort=0&photo=0&field=0&pd=10&ds={start}&de={end}&docid=&related=0&mynews=1&office_type=3&office_section_code=&news_office_checked=&nso=so%3Ar%2Cp%3Aall&is_sug_officeid=0&office_category=1&service_area=0"
-    driver.get(url)
+    entire_news_titles = set()
+    for keyword in keywords:
+        url = f"https://search.naver.com/search.naver?where=news&query={keyword}&sm=tab_opt&sort=0&photo=0&field=0&pd=10&ds={start}&de={end}&docid=&related=0&mynews=1&office_type=3&office_section_code=&news_office_checked=&nso=so%3Ar%2Cp%3Aall&is_sug_officeid=0&office_category=1&service_area=0"
+        driver.get(url)
 
-    # Scroll down until no more news exist
-    body = driver.find_element(By.TAG_NAME, "body")
-    prev_height = driver.execute_script("return document.body.scrollHeight")
-    while True:
-        body.send_keys(Keys.PAGE_DOWN)
-        time.sleep(4)
+        # Scroll down until no more news exist
+        body = driver.find_element(By.TAG_NAME, "body")
+        prev_height = driver.execute_script("return document.body.scrollHeight")
+        while True:
+            body.send_keys(Keys.PAGE_DOWN)
+            time.sleep(4)
 
-        now_height = driver.execute_script("return document.body.scrollHeight")
-        if prev_height == now_height:
-            break
-        prev_height = now_height
+            now_height = driver.execute_script("return document.body.scrollHeight")
+            if prev_height == now_height:
+                break
+            prev_height = now_height
+        # parsing when the scroll down is finished
+        soup = BeautifulSoup(driver.page_source, "lxml")
 
-    # parsing when the scroll down is finished
-    soup = BeautifulSoup(driver.page_source, "lxml")
-
-    # get the news titles
-    news_titles = [title.text.strip() for title in soup.select(".news_tit") if title.text and keyword in title.text]
-
+        # get the news titles
+        news_titles = {title.text.strip() for title in soup.select(".news_tit") if title.text and keyword in title.text}
+        entire_news_titles.update(news_titles)
+        
     driver.quit()
-    return news_titles
+    return list(entire_news_titles)
 
 @app.get("/")
 def get_news():
-    keyword = "탄핵"
-    results = crawl_news(keyword)
-    return {"keyword": keyword, "news_titles": results}
+    keywords = ["탄핵", "尹", "헌재"]
+    results = crawl_news(keywords)
+    return {"keywords": keywords, "news_titles": results}
